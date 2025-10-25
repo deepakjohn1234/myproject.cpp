6135dc35d01ca105f6bd6b54405848905d029bef
deepakjohn1234 <deepakjohn032009@gmail.com>
2025-10-25 17:06:29 +0530

    Add files via upload

---
+++ myproject.cpp
@@ -0,0 +1,16 @@
+// first 10 natural numbers
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main()
+    {
+     int a=1;
+     cout<<"first 10 natural numbers are";
+     while(a<=10)
+      {
+       cout<<a;
+       a=a+1;
+      }
+     return 0;
+    }
+     
